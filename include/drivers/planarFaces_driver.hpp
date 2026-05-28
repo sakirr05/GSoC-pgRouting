dbb393d5f527b4a999856e7ac32938dd3158bd8e
sakir05 <sakirahmed690078@gmail.com>
2026-05-28 19:04:03 +0000

    Add planarFaces skeleton structure

---
+++ include/drivers/planarFaces_driver.hpp
@@ -0,0 +1,41 @@
+/*PGR-GNU*****************************************************************
+File: planarFaces_driver.hpp
+
+Copyright (c) 2026 pgRouting developers
+Mail: project@pgrouting.org
+
+------
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+********************************************************************PGR-GNU*/
+
+#ifndef INCLUDE_DRIVERS_PLANARFACES_DRIVER_HPP_
+#define INCLUDE_DRIVERS_PLANARFACES_DRIVER_HPP_
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+#include <string>
+
+using Planar_face_rt = struct Planar_face_rt;
+
+void
+do_planarFaces(
+        std::string,
+        Planar_face_rt**, size_t*,
+        char **, char **, char **);
+
+#endif  // INCLUDE_DRIVERS_PLANARFACES_DRIVER_HPP_
---
+++ sql/planar/planarFaces.sql
@@ -0,0 +1,45 @@
+/*PGR-GNU*****************************************************************
+File: planarFaces.sql
+
+Copyright (c) 2026 pgRouting developers
+Mail: project@pgrouting.org
+
+------
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+********************************************************************PGR-GNU*/
+
+--v4.0.0
+CREATE FUNCTION pgr_planarFaces(
+    TEXT, -- edges_sql (required)
+    OUT seq BIGINT,
+    OUT face_id BIGINT,
+    OUT edge BIGINT)
+RETURNS SETOF RECORD AS
+$BODY$
+    SELECT seq, face_id, edge
+    FROM _pgr_planarFaces(_pgr_get_statement($1));
+$BODY$
+LANGUAGE SQL VOLATILE STRICT;
+
+COMMENT ON FUNCTION pgr_planarFaces(TEXT)
+IS 'pgr_planarFaces
+- EXPERIMENTAL
+- Parameters:
+  - Edges SQL with columns: id, source, target, cost [,reverse_cost]
+- Documentation:
+  - ${PROJECT_DOC_LINK}/pgr_planarFaces.html
+';
